efe5d56d5b9cf1d32f52e8e98ba21aecc75e5cda
Morningchen123 <utlkmv8312@gmail.com>
2023-03-13 12:02:22 +0800

    week 4

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
+# SubstitutionCipher Algorithm
 module SubstitutionCipher
+  # Caesar Implement
   module Caesar
     # Encrypts document using key
     # Arguments:
@@ -7,6 +11,8 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using caesar cipher
+
+      document.to_s.chars.map { |c| (c.ord + key).chr }.join
     end
 
     # Decrypts String document using integer key
@@ -16,9 +22,12 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using caesar cipher
+
+      encrypt(document, -key)
     end
   end
 
+  # Permutation Implement
   module Permutation
     # Encrypts document using key
     # Arguments:
@@ -27,6 +36,10 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+
+      shuffled_chars = (32..126).to_a.shuffle(random: Random.new(key))
+      char_map = Hash[(32..126).to_a.zip(shuffled_chars)]
+      document.to_s.chars.map { |c| char_map[c.ord].chr }.join
     end
 
     # Decrypts String document using integer key
@@ -36,6 +49,9 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+      shuffled_chars = (32..126).to_a.shuffle(random: Random.new(key))
+      char_map = Hash[shuffled_chars.zip((32..126).to_a)]
+      document.to_s.chars.map { |c| char_map[c.ord].chr }.join
     end
   end
 end
